8f519f851b81b2575e33458a1509de397fff50cd
Jvalley8 <114607646+Jvalley8@users.noreply.github.com>
2023-02-19 13:52:30 -0600

    Update New_RFID.py

--- New_RFID.py
+++ New_RFID.py
@@ -2,45 +2,35 @@ import usb.core
 import usb.util
 import time
 
-# Constants for USB communication
-VENDOR_ID = 0x16c0
-PRODUCT_ID = 0x27db
-TIMEOUT = 1000
-
-# Define the USB device
-dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+# find the J4210U RFID reader device
+dev = usb.core.find(idVendor=0x16c0, idProduct=0x27db)
 if dev is None:
     raise ValueError('Device not found')
 
-# Detach the kernel driver
-if dev.is_kernel_driver_active(0):
-    dev.detach_kernel_driver(0)
+# detach the kernel driver from the interface
+for cfg in dev:
+    for intf in cfg:
+        if dev.is_kernel_driver_active(intf.bInterfaceNumber):
+            try:
+                dev.detach_kernel_driver(intf.bInterfaceNumber)
+            except usb.core.USBError as e:
+                sys.exit("Could not detach kernel driver: %s" % str(e))
 
-# Set the configuration
-dev.set_configuration()
+# claim the device interface
+usb.util.claim_interface(dev, 0)
 
-# Get the first interface and claim it
-interface = dev.get_interface(0)
-usb.util.claim_interface(dev, interface)
+# write data to the tag
+tag_id = b'\x01\x02\x03\x04'
+tag_data = b'Hello, world!'
+write_data = tag_id + tag_data
+packet_size = 64
+num_packets = len(write_data) // packet_size + 1
+for i in range(num_packets):
+    packet_data = write_data[i*packet_size:(i+1)*packet_size]
+    packet_data += b'\x00' * (packet_size - len(packet_data))
+    dev.write(0x01, packet_data, 1000)
+    time.sleep(0.05)
 
-# Write data to the device
-endpoint_out = interface.endpoints()[0]
-data = [0x05, 0x00, 0x01, 0x00, 0x01, 0x00, 0x0b, 0x4a, 0x42, 0x3f, 0x3e, 0x3c, 0x3a, 0x32, 0x2c, 0x01]
-dev.write(endpoint_out.bEndpointAddress, data, TIMEOUT)
+# release the device interface
+usb.util.release_interface(dev, 0)
 
-# Wait for 100ms before reading data
-time.sleep(0.1)
-
-# Read data from the device
-endpoint_in = interface.endpoints()[1]
-data = dev.read(endpoint_in.bEndpointAddress, endpoint_in.wMaxPacketSize, TIMEOUT)
-
-# Print the data received
-print(data)
-
-# Release the interface and reattach the kernel driver
-usb.util.release_interface(dev, interface)
-dev.attach_kernel_driver(0)
-
-# Detach the USB device
-usb.util.dispose_resources(dev)
