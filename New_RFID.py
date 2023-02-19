d9f0724279ff4f647ca649bd1b8eb1c696a3d554
Jvalley8 <114607646+Jvalley8@users.noreply.github.com>
2023-02-19 13:58:50 -0600

    Update New_RFID.py

--- New_RFID.py
+++ New_RFID.py
@@ -1,36 +1,28 @@
 import usb.core
 import usb.util
-import time
+import sys
 
-# find the J4210U RFID reader device
-dev = usb.core.find(idVendor=0x16c0, idProduct=0x27db)
+# Check Python version compatibility
+if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 4):
+    raise Exception("PyUSB requires at least Python 3.4")
+
+# Find J4210U device
+dev = usb.core.find(idVendor=0x1c34, idProduct=0x724e)
+
+# Check if device is found
 if dev is None:
-    raise ValueError('Device not found')
+    raise Exception("J4210U device not found")
 
-# detach the kernel driver from the interface
-for cfg in dev:
-    for intf in cfg:
-        if dev.is_kernel_driver_active(intf.bInterfaceNumber):
-            try:
-                dev.detach_kernel_driver(intf.bInterfaceNumber)
-            except usb.core.USBError as e:
-                sys.exit("Could not detach kernel driver: %s" % str(e))
+# Set configuration
+dev.set_configuration()
 
-# claim the device interface
-usb.util.claim_interface(dev, 0)
+# Write to tag
+ep_out = dev[0][(0, 0)][0]
+ep_out.write(b'\x00\x00\x00\x00\x02\x05\x00\x00\x00')
 
-# write data to the tag
-tag_id = b'\x01\x02\x03\x04'
-tag_data = b'Hello, world!'
-write_data = tag_id + tag_data
-packet_size = 64
-num_packets = len(write_data) // packet_size + 1
-for i in range(num_packets):
-    packet_data = write_data[i*packet_size:(i+1)*packet_size]
-    packet_data += b'\x00' * (packet_size - len(packet_data))
-    dev.write(0x01, packet_data, 1000)
-    time.sleep(0.05)
+# Read tag response
+ep_in = dev[0][(0, 0)][1]
+response = ep_in.read(8)
 
-# release the device interface
-usb.util.release_interface(dev, 0)
-
+# Print response
+print(response)
