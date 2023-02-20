4a7b9fc0d4a2d84d687d4a8c1706a851aee57fad
Jvalley8 <114607646+Jvalley8@users.noreply.github.com>
2023-02-20 15:36:02 -0600

    Update New_RFID.py

--- New_RFID.py
+++ New_RFID.py
@@ -1,30 +1,38 @@
 import serial
+import time
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+# Open serial port
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 
-# Reset the reader
-ser.write(b'\x02\x00\x00\x03\x00\x01\xB5\xCD')
-response = ser.read(10)
-print(response)
+# Reset the module
+ser.write(b'\xA0\x03\x01\x00\xA2')
+response = ser.read(7)
+
+if response != b'\xA0\x03\x00\x00\xA3\xF7\xC0':
+    print('Error resetting module')
+    ser.close()
+    exit()
 
 # Set the region to North America
-ser.write(b'\x02\x01\x07\x03\x01\x6A\xBB\xB5')
-response = ser.read(10)
-print(response)
+ser.write(b'\xA0\x04\x01\x01\x01\xA8')
+response = ser.read(7)
 
-# Set the protocol to EPC Gen2
-ser.write(b'\x02\x01\x08\x03\x01\x6B\xDA\x5E')
-response = ser.read(10)
-print(response)
+if response != b'\xA0\x04\x00\x00\xA4\xD4\x38':
+    print('Error setting region')
+    ser.close()
+    exit()
 
-# Enable the antenna
-ser.write(b'\x02\x01\x0A\x03\x01\x00\xCE\xCF')
-response = ser.read(10)
-print(response)
+# Write the EPC to tag
+epc = 'E280110B0064015017DC93E0'
+ser.write(bytes.fromhex('A0 23 03 01 00 ' + '{:02X}'.format(len(epc) // 2) + ' ' + epc))
+response = ser.read(7)
 
-# Write to tag
-ser.write(b'\x02\x01\x21\x07\x01\x00\x01\x03\xE8\x00\x01\xD0\x13')
-response = ser.read(10)
-print(response)
+if response != b'\xA0\x23\x00\x00\xA3\x13\x60':
+    print('Error writing EPC')
+    ser.close()
+    exit()
 
+print('Tag written successfully')
+
+# Close serial port
 ser.close()
