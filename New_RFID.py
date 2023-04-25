8ba8c0ef39d2a88861cc4cd11d0da51ae678e406
Jvalley8 <114607646+Jvalley8@users.noreply.github.com>
2023-04-25 15:59:41 -0500

    Update New_RFID.py

--- New_RFID.py
+++ New_RFID.py
@@ -1,27 +1,24 @@
-
+import pymysql
 import serial
-import RPi.GPIO as GPIO
 
-# Set up the serial connection
-ser = serial.Serial(
-    port='/dev/serial0', # serial port connected to the M6E Nano reader
-    baudrate=115200,     # baud rate for the serial connection
-    timeout=1            # timeout for the serial read
-)
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
 
-# Set up the GPIO pins for the M6E Nano reader
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)  # GPIO 18 controls the M6E Nano's EN pin
-GPIO.output(18, GPIO.HIGH)  # enable the M6E Nano
+try:
+    db = pymysql.connect(host='172.20.10.11',
+                         user='jevin',
+                         password='password',
+                         database='GarbageDB')
+except Exception as e:
+    print ("Error:",e)
 
-# Send the command to read a tag
-ser.write(bytes.fromhex('A000300B9163960080')) # EPC Gen2 Read Tag Command
+print("Connection Successful")
 
-# Read the response from the M6E Nano
-response = ser.read(128)
+cursor = db.cursor()
+sql = "INSERT INTO RFID (EPC) VALUES (%s)"
+print("Put Tag Near Reader")
 
-# Print the response
-print(response)
-
-# Disable the M6E Nano
-GPIO.output(18, GPIO.LOW)
+while True:
+    data = ser.readline().decode()          ##.decode().rstrip()
+    if data:
+        cursor.execute(sql, (data))
+        db.commit()
